7ce08b0accd8453156926cbef9152a35cb3d7f07
Florencekyarikunda <florencekyarikund@gmail.com>
2021-02-21 09:54:05 +0300

    first commit

---
+++ src/hello2.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val name="florence kyarikunda"
+    println(name)
+
+    var age= 21
+    println(age)
+
+     val phoneNumber = "0777726064"
+      println(phoneNumber)
+
+    var weight : Int = 60
+    println(weight)
+
+    val citizen : String = "Ugandan"
+    println(citizen)
+}
